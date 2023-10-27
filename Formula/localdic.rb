406563dd365339706dfb17ded1a6f80a988051a1
Romona Mcgunnigle <caishilin@yahoo.com>
2023-10-27 13:43:41 +0800

    Update localdic.rb

--- Formula/localdic.rb
+++ Formula/localdic.rb
@@ -1,9 +1,9 @@
 class Localdic < Formula
   desc "Manage the local dictionary on Mac."
   homepage "https://github.com/SSBun/localdic"
-  url "https://github.com/SSBun/localdic.git", tag: "0.1"
-  version "0.1"
-  sha256 "daa4ad0964a613afcd0d053e74fe75aa7f029cdb18dab7f5e240ede12ecffb1b"
+  url "https://github.com/SSBun/localdic.git", tag: "0.1.1"
+  version "0.1.1"
+  #sha256 "daa4ad0964a613afcd0d053e74fe75aa7f029cdb18dab7f5e240ede12ecffb1b"
 
   depends_on "xcode": [:build]
 
